8818d90845fe4970f0258267f4918f08f78cb3d0
Tim Martinez <83369306+timperry151@users.noreply.github.com>
2023-05-15 20:54:05 -0400

    add prompt for user

---
+++ script.js
@@ -0,0 +1,7 @@
+// prompt user for selection
+let player = prompt("Rock, Paper, Scissors?")
+
+console.log(player)
+// randomly generate computer choice
+// play a round against computer
+// return results
